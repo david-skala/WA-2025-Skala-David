d554770be3fe4bba5c1cfe0c8c576bc443e99bb2
David Skála <david.skala@tul.cz>
2025-04-10 11:52:41 +0200

    #7 hezci tabulka

--- 03-php-projekt/app/views/books/books_edit_delete.php
+++ 03-php-projekt/app/views/books/books_edit_delete.php
@@ -37,7 +37,7 @@ if (isset($_GET['edit'])) {
     <h2>Výpis knih</h2>
     <?php if (!empty($books)): ?>
         <table class="table table-bordered table-hover">
-            <thead class="table-primary">
+            <thead class="table-info text-center">
                 <tr>
                     <th>ID</th>
                     <th>Název</th>
@@ -51,7 +51,7 @@ if (isset($_GET['edit'])) {
             </thead>
             <tbody>
             <?php foreach ($books as $book): ?>
-                <tr>
+                <tr class="text-center">
                     <td><?= htmlspecialchars($book['id']) ?></td>
                     <td><?= htmlspecialchars($book['title']) ?></td>
                     <td><?= htmlspecialchars($book['author']) ?></td>
@@ -60,7 +60,7 @@ if (isset($_GET['edit'])) {
                     <td><?= number_format($book['price'], 2, ',', ' ') ?> Kč</td>
                     <td><?= htmlspecialchars($book['isbn']) ?></td>
                     <td>
-                        <a href="?edit=<?= $book['id'] ?>" class="btn btn-sm btn-warning">Upravit</a>
+                        <a href="?edit=<?= $book['id'] ?>" class="btn btn-sm btn-outline-secondary">Upravit</a>
                         <a href="../../controllers/book_delete.php?id=<?= $book['id'] ?>" class="btn btn-sm btn-danger" onclick="return confirm('Opravdu chcete smazat tuto knihu?');">Smazat</a>
                     </td>
                 </tr>
