6d9054f88cf9c1d712267bb3add1ae2682e2caff
David Skála <david.skala@tul.cz>
2025-04-17 11:59:58 +0200

    #8 databaze s users

--- 03-php-projekt/wa_2025_ds_01.sql
+++ 03-php-projekt/wa_2025_ds_01.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Počítač: 127.0.0.1
--- Vytvořeno: Čtv 20. bře 2025, 12:13
+-- Vytvořeno: Čtv 17. dub 2025, 11:59
 -- Verze serveru: 10.4.32-MariaDB
 -- Verze PHP: 8.2.12
 
@@ -55,6 +55,34 @@ INSERT INTO `books` (`id`, `title`, `author`, `category`, `subcategory`, `year`,
 (5, 'fgfdgd', 'fdgdfgdf', '', NULL, 20005, 5.00, '555454', '', '', '[]', '2025-03-20 11:01:48', '2025-03-20 11:01:48'),
 (7, 'ůhhgfh', 'gfhfghgfh', '', NULL, 200, 200.00, '20000', '', '', '[]', '2025-03-20 11:04:59', '2025-03-20 11:04:59');
 
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `role` enum('user','admin') NOT NULL DEFAULT 'user',
+  `name` varchar(100) DEFAULT NULL,
+  `surname` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Vypisuji data pro tabulku `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password_hash`, `role`, `name`, `surname`, `created_at`, `updated_at`) VALUES
+(1, 'xxxx', 'dmflmfld@gmail.com', '48548', 'user', 'd', 's', '2025-04-17 09:37:41', NULL),
+(2, '2', '2@gmail.com', '48548', 'user', 'dSS', 'sadfadf', '2025-04-17 09:38:43', NULL),
+(3, 'ksjksfsa', 'fgsfg', '12345', 'user', 'fgsfg', 'fgsfg', '2025-04-17 09:41:24', NULL),
+(5, 'pjdgipdjfgidjgpida', 'fgsfg', '12345', 'user', 'fgsfg', 'fgsfg', '2025-04-17 09:42:37', NULL);
+
 --
 -- Indexy pro exportované tabulky
 --
@@ -67,6 +95,13 @@ ALTER TABLE `books`
   ADD UNIQUE KEY `isbn` (`isbn`);
 
 --
+-- Indexy pro tabulku `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
 -- AUTO_INCREMENT pro tabulky
 --
 
@@ -75,6 +110,12 @@ ALTER TABLE `books`
 --
 ALTER TABLE `books`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pro tabulku `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
